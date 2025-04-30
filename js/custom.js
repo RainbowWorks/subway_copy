ed512fe880f48339a3587c128905ab16ab054fd3
RainbowWorks <kuj4235@kakao.com>
2025-04-30 16:20:26 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,21 @@ $(function () {
         parallax: true,/*겹치는 슬라이드이다. wiper-parallax="300" html에 적어줘야됨*/
         speed: 1000,/*옮기는거1초*/
     });/*슬라이드 돌아감, 베껴쓰면된다.*/
+
+    const MIS = new Swiper('.main_news_slide', {
+        loop: true,
+        effect: 'fade', /*이미지에fade주기(effect: 'fade',)*/
+        pagination: {
+            el: '.main_news .page',
+            clickable: true, /*클릭안되면 쓰면된다.*/
+        },/*도트만들때필요함*/
+        navigation: {
+            nextEl: '.main_news .next',
+            prevEl: '.main_news .prev',
+        },/*화살표*/
+    });
+
+
 });
 
 $(function () {
@@ -19,14 +34,14 @@ $(function () {
 });
 
 $(function () {
-    $('.main_content .tab_menu button').on('click', function () {
+    $('.main_menu .tab_menu button').on('click', function () {
         let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
         console.log(idx);
 
-        $('.main_content .tab_menu button').removeClass('on');
+        $('.main_menu .tab_menu button').removeClass('on');
         $(this).addClass('on');
 
-        $('.main_content .tab_content .con').removeClass('on');
-        $('.main_content .tab_content .con').eq(idx).addClass('on');
+        $('.main_menu .tab_menu_content .con').removeClass('on');
+        $('.main_menu .tab_menu_content .con').eq(idx).addClass('on');
     });
 });
